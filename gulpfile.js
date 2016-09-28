5836bfad9a00ec12a10c4f431a5b10f98a9b5c64
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-09-28 23:45:56 +0300

    first comit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by SergST on 28.09.2016.
+ */
+
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass'); // Подключаем Sass пакет
+
+gulp.task('sass', function() { // Создаем таск "sass"
+	return gulp.src(['src/style/sass/*.sass', 'src/style/sass/*.scss']) // Берем источник
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(gulp.dest('src/style/css')); // Выгружаем результата в папку css
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['src/style/sass/*.sass', 'src/style/sass/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
+});
+
+gulp.task('default', ['watch']);
