33ff26b61b370261eaec76ffe3c318dabbf6a730
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-10-04 23:54:20 +0300

    add sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,38 +2,70 @@
  * Created by SergST on 30.09.2016.
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
+
+	//================== Carousel ===============
 
 	$("#owl-example").owlCarousel({
 
 		// Most important owl features
-		items : 3,
-		itemsCustom : false,
-		itemsDesktop : [1199,4],
-		itemsDesktopSmall : [980,3],
-		itemsTablet: [768,2],
+		items: 3,
+		itemsCustom: false,
+		itemsDesktop: [1199, 4],
+		itemsDesktopSmall: [980, 3],
+		itemsTablet: [768, 2],
 		itemsTabletSmall: false,
-		itemsMobile : [479,1],
-		singleItem : true,
-		itemsScaleUp : false,
+		itemsMobile: [479, 1],
+		singleItem: true,
+		itemsScaleUp: false,
 
 		//Basic Speeds
-		slideSpeed : 200,
-		paginationSpeed : 800,
-		rewindSpeed : 1000,
+		slideSpeed: 200,
+		paginationSpeed: 800,
+		rewindSpeed: 1000,
 
 		//Autoplay
-		autoPlay : false,
-		stopOnHover : true,
+		autoPlay: false,
+		stopOnHover: true,
 
 		//Pagination
-		pagination : true,
+		pagination: true,
 		paginationNumbers: false,
 
 		// Responsive
 		responsive: true,
-		responsiveRefreshRate : 200,
+		responsiveRefreshRate: 200,
 		responsiveBaseWidth: window
+	});
+
+	//============Accordeon=================
+
+	var $accHead = $('.accordeon .accordeon-item__header');
+	var $accArticle = $('.accordeon .accordeon-item__article');
+
+	function hide(a, b) {
+		a.slideUp('slow');
+		b.removeClass('accordeon-item__header--open')
+	}
+
+	hide($accArticle, $accHead);
+
+	$accHead.eq(0)
+		.addClass('accordeon-item__header--open')
+		.siblings('.accordeon-item__article')
+		.slideDown('slow');
+
+	$accHead.click(function () {
+
+		if ($(this).hasClass('accordeon-item__header--open')) {
+			hide($(this).siblings('.accordeon-item__article'), $(this));
+		}
+		else {
+			hide($accArticle, $accHead);
+			$(this).toggleClass('accordeon-item__header--open')
+				.siblings('.accordeon-item__article')
+				.slideDown('slow');
+		}
 	});
 
 });
